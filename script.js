a88e945b21acca04775bdf9e65dd333c6becda74
jeerawitch treesukoun <jeerawitch@192.168.0.2>
2024-01-07 00:24:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = function () {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
